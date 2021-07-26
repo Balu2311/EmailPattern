6f8494095beaca1f25f878adb13aa077200e3f5b
Vallem Balu <vbnreddy5@gmail.com>
2021-07-26 16:15:10 +0530

    Sample Emails to Test

---
+++ EmailPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EmailPattern
+{
+    class Program
+    {
+        public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome_To_Email Pattern");
+            EmailValidation();
+        }
+        public static void EmailValidation()
+        {
+            string EMAIL_PATTERN = "^[A-Z0-9a-z]{1,}([.#$^+-][A-Za-z0-9]+)*[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+            Regex rg = new Regex(EMAIL_PATTERN);
+            Console.Write("Enter E-mail: ");
+            string email = Console.ReadLine();
+            bool validate = rg.IsMatch(email);
+            if (validate)
+                Console.WriteLine("Email is valid : " + email);
+            else
+                Console.WriteLine("Invalid E-mail.");
+        }
+    }
+}
